2752b40ba21834e477d2241ce4d5042673ff263e
EMEKS <Chukwuemeka.collins@binghamuni.edu.ng>
2026-03-31 01:29:42 +0800

    Update game.py

--- game.py
+++ game.py
@@ -1,6 +1,6 @@
 import random
 
-top_of_range= input("Type a number")
+top_of_range= input("Type a number: ")
 
 if top_of_range.isdigit():
     top_of_range = int(top_of_range)
@@ -15,11 +15,11 @@ else:
 random_number=random.randint(0, top_of_range)
 guesses = 0
 
-print(random_number)
+
 
 while True:
     guesses += 1
-    user_guess = input("Make a guess")
+    user_guess = input("Make a guess: ")
     if user_guess.isdigit():
         user_guess = int(user_guess )
     else:
